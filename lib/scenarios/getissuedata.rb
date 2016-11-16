357d406502df158c992615288bdc8e3d1f1c95d0
Alexey Sychev <alexey.sychev@onetwotrip.com>
2016-11-16 12:25:04 +0300

    Add new scenario - getissuedata

---
+++ lib/scenarios/getissuedata.rb
@@ -0,0 +1,75 @@
+module Scenarios
+  ##
+  # GetIssueData scenario
+  class GetIssueData
+    def run
+      # repo to module dictionary
+      modules_dict = {
+        'avia' => 'back_avia',
+        'seo_pages' => 'back_seopages',
+        'bundle-back' => 'back_bundles',
+        '12trip-railways.node' => 'back_blackbox',
+        '12trip' => 'front_12trip',
+        'twiket_backoffice' => 'front_bo',
+        'twiket-live' => 'front_tlive',
+        '12trip_hotels' => 'front_hotels',
+        'm-hotels' => 'front_mhotels',
+        'm-12trip' => 'front_mobile',
+        'emails' => 'front_emails',
+        'xjsx' => 'front_xjsx',
+      }
+      raise 'No ENVIRONMENT!' unless ENV['ENVIRONMENT']
+      jira = JIRA::Client.new SimpleConfig.jira.to_h
+      issue = jira.Issue.find SimpleConfig.jira.issue
+
+      # Get unique labels from release issue and all linked issues
+      labels = issue.labels
+      issue.linked_issues('deployes').each do |linked_issue|
+        labels.concat(linked_issue.labels)
+      end
+      labels = labels.uniq
+
+      prs = issue.related['pullRequests']
+
+      puts 'Checking for wrong PRs names:'
+
+      prs.each do |pr|
+        prname = pr['name'].dup
+        if pr['name'].strip!.nil?
+          puts "[#{prname}] - OK"
+        else
+          puts "[#{prname}] - WRONG! Stripped. Bad guy: #{pr['author']['name']}"
+        end
+      end
+
+      git_style_release = SimpleConfig.jira.issue.tr('-', ' ').downcase.capitalize
+
+      prs.select! { |pr| (/^((#{SimpleConfig.jira.issue})|(#{git_style_release}))/.match pr['name']) && pr['status'] != 'DECLINED' }
+
+      if prs.empty?
+        puts 'No pull requests for this task!'
+        exit 1
+      end
+
+      puts 'Selected PRs:'
+      puts prs.map { |pr| pr['name'] }
+
+      prop_values = { 'ENVIRONMENT' => ENV['ENVIRONMENT'] }
+
+      prs.each do |pr|
+        repo_name = pr['url'].split('/')[-3]
+        unless pr['destination']['branch'].include? 'master'
+          puts "WTF? Why is this Pull Request here? o_O (destination: #{pr['destination']['branch']}"
+          next
+        end
+        prop_values["#{modules_dict[repo_name].upcase}_REVISION"] = pr['source']['branch']
+        project_labels = labels.select { |label| label.start_with? "#{repo_name}_" }.map { |label| label.remove("#{repo_name}_") }
+        prop_values["#{repo_name.upcase}_LABELS"] = project_labels.join(',') unless project_labels.empty?
+      end
+
+      JavaProperties.write prop_values, './.issue_data'
+
+      exit 0
+    end
+  end
+end
